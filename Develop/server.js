e94748969a5357ccf853fa6c936a6c01233708e0
js131993 <john.sanders4444@gmail.com>
2023-07-11 14:15:33 -0400

    delete route....
    I have created the delete

--- Develop/server.js
+++ Develop/server.js
@@ -16,15 +16,22 @@ app.get("/notes", (req, res) =>
 );
 // app.get('/notes', (req, res) => res.sendFile('./public/notes.html'));
 
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/api/notes/:id', (req, res) => res.json(noteData));
 //returning all information and notes..
 
 //access the body variable... 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes/:id', (req, res) => {
     noteData.push(req.body);
     fs.writeFile("./db/db.json", JSON.stringify(noteData));
 });
 //fs.writefile is similar to saving to database.
+
+app.delete('/api/notes/:id', (req, res) => {
+  noteData.pop(req.body);
+  fs.writeFile("./db/db.json", JSON.stringify(noteData));
+});
+//Should I have fs.writeFile in my delete route above?
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
